5f5b01672b3e7441b49caef4e84b84da05be45ba
wander <wander@ffactory.org>
2021-08-08 20:16:00 +0800

    Create main.go
    测试代码

---
+++ test/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/obity/pretree"
+)
+
+func main() {
+	getTree := pretree.GetTreeByMethod(pretree.HttpMethod("GET"))
+	getTree.Insert("account/:id/info/:2333")
+	getTree.Insert("account/:id/login")
+	getTree.Insert("account/:id")
+	getTree.Insert("bacteria/count_number_by_month")
+
+	list := []string{"account/929239", "account/9929s/login", "account/safsd32/info/121323", "bacteria/count_number_by_month"}
+	for _, v := range list {
+		ok, _ := getTree.Match(v)
+		fmt.Printf("%s:%t\n", v, ok)
+	}
+}
