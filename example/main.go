077634606c22ac8b546f121be33abf8bf8e5c620
wander <wander@ffactory.org>
2021-08-12 21:49:39 +0800

    返回路由中的变量vars
    Query查询函数，URL匹配同时返回变量名和对应值在map中

--- example/main.go
+++ example/main.go
@@ -18,7 +18,7 @@ import (
 )
 
 func main() {
-	pretree.Store(pretree.MethodGet, "account/{id}/info/:2333")
+	pretree.Store(pretree.MethodGet, "account/{id}/info/:name")
 	pretree.Store(pretree.MethodGet, "account/:id/login")
 	pretree.Store(pretree.MethodGet, "account/{id}")
 	pretree.Store(pretree.MethodGet, "bacteria/count_number_by_month")
@@ -29,12 +29,12 @@ func main() {
 		"bacteria/count_number_by_mont",
 	}
 	for _, v := range list {
-		ok, tree := pretree.Query(pretree.MethodGet, v)
+		ok, tree, vars := pretree.Query(pretree.MethodGet, v)
 		if ok {
-			fmt.Printf("rule: %s url: %s result:%v\n", tree.Rule(), v, ok)
+			fmt.Printf("rule: %s url: %s result:%v vars: %v\n", tree.Rule(), v, ok, vars)
 
 		} else {
-			fmt.Printf("rule: %s url: %s result:%v\n", "", v, ok)
+			fmt.Printf("rule: %s url: %s result:%v vars: %v\n", "", v, ok, vars)
 		}
 	}
 }
